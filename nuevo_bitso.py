f160e3c2ec15476359a64694e59fe5fca9448e6e
Miguel <garcia.migue.onate@gmail.com>
2020-04-07 14:46:10 -0500

    ya cambia de colorsito

--- nuevo_bitso.py
+++ nuevo_bitso.py
@@ -1,6 +1,5 @@
 import requests
-import colored
-from colored import stylize
+import sys
 URL = 'https://api.bitso.com/v3/ticker/'
 
 def ultimo_precio_criptomoneda():
@@ -13,13 +12,14 @@ def main():
     ultimo_precio = -1
     while True:
         precio_actual = ultimo_precio_criptomoneda()
+        precio_actual = round(precio_actual,2)
 
         if precio_actual != ultimo_precio:
             if precio_actual < ultimo_precio:
-                print(' BTC: ',round(ultimo_precio,2))
+                print('\033[1;31;40m BTC: ' + str(precio_actual))
                 ultimo_precio = precio_actual
             else:
-                print(' BTC: ',round(ultimo_precio,2))
+                print('\033[1;32;40m BTC: ' + str(precio_actual))
                 ultimo_precio = precio_actual
 
 main()
