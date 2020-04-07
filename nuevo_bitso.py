35b02f7d344c06eefcfa8ac8e29bfcf97fae33d8
Miguel <garcia.migue.onate@gmail.com>
2020-04-07 16:51:23 -0500

    holis

--- nuevo_bitso.py
+++ nuevo_bitso.py
@@ -5,10 +5,10 @@ URL = 'https://api.bitso.com/v3/ticker/'
 def ultimo_precio_criptomoneda():
     respuesta = requests.get(URL)
     respuesta_json = respuesta.json()
-    return float(respuesta_json['payload'][0]['vwap'])
+    return float(respuesta_json['payload'][0]['ask'])
 
 def main():
-
+    print('\n\tConectando con el servidor de BITSO\n')
     ultimo_precio = -1
     while True:
         precio_actual = ultimo_precio_criptomoneda()
@@ -16,10 +16,10 @@ def main():
 
         if precio_actual != ultimo_precio:
             if precio_actual < ultimo_precio:
-                print('\033[1;31;40m BTC: ' + str(precio_actual))
+                print('\033[1;31;40m \tBTC: $ ' + str(precio_actual) + " MXN")
                 ultimo_precio = precio_actual
             else:
-                print('\033[1;32;40m BTC: ' + str(precio_actual))
+                print('\033[1;32;40m \tBTC: $ ' + str(precio_actual) + " MXN")
                 ultimo_precio = precio_actual
 
 main()
