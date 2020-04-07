7b96086eeeb24c27b1bacd48a013cac3c8d58d1e
Miguel <garcia.migue.onate@gmail.com>
2020-04-07 18:39:28 -0500

    fj

---
+++ BITSO/priv_REST_API_Balance_Info.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import requests
+import sys
+import time
+import hmac
+import hashlib
+import requests
+import json
+
+bitso_key = 'NjeZbAXHKy'
+bitso_secret = '4fd5e392f1ab9381bd26939617110dd0'
+http_method = "GET"
+request_path = "/v3/balance/"
+
+ # nonce genera un numero unico y siempre creciente
+ # basado en el tiempo necesario para cada vez que
+# llamamos a la API.
+nonce = str(int(round(time.time() * 1000)))
+message = nonce+http_method+request_path
+
+# el signature debe ser generado creando una SHA256 HMAC usando el API Secret
+signature = hmac.new(bitso_secret.encode('utf-8'),
+                     message.encode('utf-8'),hashlib.sha256).hexdigest()
+
+# las reglas para autorizar en bitso son usando el authorization header
+# que se forma de la siguiente manera:
+auth_header = 'Bitso %s:%s:%s' % (bitso_key,nonce,signature)
+
+response = requests.get('https://api.bitso.com' + request_path, headers={"Authorization": auth_header})
+respone_json = response.json()
+
+print response_json['success']
