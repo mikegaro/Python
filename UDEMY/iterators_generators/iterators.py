c3f00ea5b876a8bff1a75d974d0ec01018298b58
Mike <garcia.miguel.onate@gmail.com>
2021-08-10 18:34:45 -0500

    hola

---
+++ UDEMY/iterators_generators/iterators.py
@@ -0,0 +1,32 @@
+# for num in [1,2,3]
+# for char in "hi_there"
+
+def my_for(iterable, func):
+    iterator = iter(iterable)
+    while True:
+        try:
+            thing = next(iterator)
+        except StopIteration:
+            break
+        else:
+            func(thing)
+
+
+class Counter:
+    def __init__(self, low, high) -> None:
+        self.current = low
+        self.high = high
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < self.high:
+            num = self.current
+            self.current += 1
+            return num
+        raise StopIteration
+
+
+for x in Counter(50, 700):
+    print(x)
